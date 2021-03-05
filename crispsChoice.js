77329db0638972350a66ea8c3f0263500217f4a1
simonScott1302 <80096257+simonScott1302@users.noreply.github.com>
2021-03-05 10:26:40 +0000

    Add files via upload

---
+++ crispsChoice.js
@@ -0,0 +1,20 @@
+let  randomNum = Math.floor(Math.random() * 5);
+
+switch (randomNum) {
+    case 0:
+        console.log(`Computer picked ${randomNum}. You can have Salt and Vingar crisps today.`);
+        break;
+    case 1:
+        console.log(`Computer picked ${randomNum}. You can have Cheese and Onion crisps today.`)
+        break;
+    case 2:
+        console.log(`Computer picked ${randomNum}. You can have Scampi Fries today.`)
+        break;
+    case 3:
+        console.log(`Computer picked ${randomNum}. You can have Bacon Fries today.`)
+        break;
+    case 4: 
+        console.log(`Computer picked ${randomNum}. You can have Salt and Shake crisps today.`)
+    
+}
+
